e36ac12845c70ff449727bb593aee41a03316ee3
Abi <abi@LAPTOP-6TQM1T50.localdomain>
2023-09-28 11:54:10 +0530

    prepare callbacks completed

--- src/Chicken_Disease_Classification/config/configuration.py
+++ src/Chicken_Disease_Classification/config/configuration.py
@@ -1,6 +1,6 @@
 from Chicken_Disease_Classification.constants import *
 from Chicken_Disease_Classification.utils.common import read_yaml, create_directories
-from Chicken_Disease_Classification.entity import DataIngestionConfig
+from Chicken_Disease_Classification.entity import *
 
 class ConfigurationManager:
     def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH):
@@ -39,3 +39,17 @@ class ConfigurationManager:
         )
 
         return get_prepare_base_model_config
+    
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([Path(model_ckpt_dir), Path(config.tensorboard_root_log_dir)])
+
+        prepare_callbacks_config = PrepareCallbacksConfig(
+            root_dir = Path(config.root_dir),
+            tensorboard_root_log_dir = Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath = config.checkpoint_model_filepath
+        )
+
+        return prepare_callbacks_config
